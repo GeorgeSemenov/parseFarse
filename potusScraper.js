911bfb53bec1a8c870f67254a985e8d7d5e44ed0
GeorgeSemenov <zhora.sem@yandex.ru>
2021-04-02 13:41:12 +0300

    cho cho, oppa nifiga

---
+++ potusScraper.js
@@ -0,0 +1,29 @@
+//https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
+fs = require('fs');//Библотека fileStream - позволяет работать с файлами на твоём компуке.
+const ch = require('cheerio');//Библиотека, позволяющаяя непосредственно парсить (перемещаться
+                             // между тэгами используя селекторы CSS)
+const rp = require('request-promise');//Библиотека, которая позволяет делать запросы
+                                      //На страницы в интернете и тянуть информацию
+                                      //оттуда
+const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+
+rp(url)//Заставляем парсить по указанному URL
+  .then(function(html){//В случае успеха - запихиваем содержимое страницы в файл pareResult
+    //success!
+    const wikiUrls = [];
+    const wikiNames = [];
+    let arrLength = ch('b > a', html).length;
+    for (let i = 0; i < arrLength; i++) {
+      // console.log(`arrLength = ${arrLength}\ni = ${i}\ni < arrLength = ${i < arrLength}`);
+      // wikiUrls.push( ch('b > a', html)[i].attribs.href );
+      // wikiNames.push(( ch('b > a', html)[i].texttextContent ));
+    }
+    console.log('b > a = ' + (ch('b > a' , html)).text())
+    fs.writeFile('parseResult.txt', (ch('b > a' , html)[30]).text(), function (err) {
+      if (err) return console.log(err);
+    });
+    //console.log(ch('b > a', html));//выводим спарщенные ссылки с президенатми на экран
+  })
+  .catch(function(err){
+    //handle error
+  });
