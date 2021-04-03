c1f14b6fe3909d13269d12f3260fd3a7b32c96e8
George Semenov <zhora.sem@yandex.ru>
2021-04-03 10:12:00 +0300

    it's almost donw

--- potusScraper.js
+++ potusScraper.js
@@ -1,10 +1,14 @@
 //https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
+//npm install --save request request-promise cheerio puppeteer
 fs = require('fs');//Библотека fileStream - позволяет работать с файлами на твоём компуке.
-const ch = require('cheerio');//Библиотека, позволяющаяя непосредственно парсить (перемещаться
+const $ = require('cheerio');//Библиотека, позволяющаяя непосредственно парсить (перемещаться
                              // между тэгами используя селекторы CSS)
 const rp = require('request-promise');//Библиотека, которая позволяет делать запросы
                                       //На страницы в интернете и тянуть информацию
                                       //оттуда
+const parseName = require('./parseName.js');//Подключаем наш модуль
+                                          //по парсингу имён и днов
+                                          //рожденньйов
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 rp(url)//Заставляем парсить по указанному URL
@@ -12,18 +16,49 @@ rp(url)//Заставляем парсить по указанному URL
     //success!
     const wikiUrls = [];
     const wikiNames = [];
-    let arrLength = ch('b > a', html).length;
-    for (let i = 0; i < arrLength; i++) {
-      // console.log(`arrLength = ${arrLength}\ni = ${i}\ni < arrLength = ${i < arrLength}`);
-      // wikiUrls.push( ch('b > a', html)[i].attribs.href );
-      // wikiNames.push(( ch('b > a', html)[i].texttextContent ));
+    let arrLength = $('b > a', html).length;
+    for (let i = 0; i < 10; i++) {
+      wikiUrls.push( $('b > a', html)[i].attribs.href );//Собираем массив ссылок
+      // wikiNames.push(( $('b > a', html)[i].attribs.title));//Обрати внимание
+                                    //Что у ссылок нельзя извлечь текст
+                                    //методом text() это делается через
+                                    //свойство title
     }
-    console.log('b > a [3] = ' + ch('b > a' , html)) 
-    fs.writeFile('parseResult.txt', (ch('b > a' , html)[15]).text, function (err) {
-      if (err) return console.log(err);
-    });
-    //console.log(ch('b > a', html));//выводим спарщенные ссылки с президенатми на экран
+    //console.log($('b > a', html));//выводим спарщенные ссылки с президенатми на экран
+    return Promise.all(/*promise.all - получает массив или друой итерируемый объект
+                        затем проходится по всем асинхронным
+                        действиям(промисам) и выполняет их, возвращает
+                        промис
+                      */
+      wikiUrls.map(function(url) {/*метод map создаёт новый массив
+                                    изменённый указанной функцией
+                                  */
+        return parseName('https://en.wikipedia.org' + url);
+                          /*в данном случае т.к. аттрибут href 
+                            в спарщенных ссылках содержит не 
+                            полный адресс, то указанная функция
+                            добавляет недостающий кусочек адресса
+                            , затем передаём эту ссылку в самодельный
+                            модуль parseName, который делает асинхронный
+                            запрос и возвращает promise/
+                            Таким образом у нас получает массив промисов,
+                            который обрабатывается и возвращается результативный
+                            промис.
+                          */
+      })
+    );
   })
-  .catch(function(err){
-    //handle error
+  .then(function(presidents) {/*Далее, результативный промис, в случае
+                                успеха (fullfiled) передаёт обработанные
+                                результаты массива промисов в свою внутреннюю 
+                                функцию
+                              */
+    console.log(presidents);// выводим массив результатов в консоль
+    fs.writeFile('parseResult.txt', JSON.stringify(presidents), function (err) {// Сохраняем данные в файл
+      if (err) return console.log(err);//Сохраняем ошибку, если таковая будет
+    });
+  })
+  .catch(function(err){//Если результативный массив завершится ошибкой
+                       //то она будет выведенна на экран.
+    console.log(err);
   });
