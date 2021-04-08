2f5e235aa1c2528d25a64a770b68bf4f681d5e60
GeorgeSemenov <zhora.sem@yandex.ru>
2021-04-08 13:34:48 +0300

    it's mouse time

--- grabber.js
+++ grabber.js
@@ -16,15 +16,23 @@ const url = 'https://www.fl.ru/projects/';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
+  await page.pdf({path: './page-before-click.pdf'})
   await page.click('.b-combo__arrow');
+  // const element = await page.$('.b-combo__arrow'); //Две следующие строки и строка выше - делают одно и тоже
+  // await element.click();                           // Разница лишь в том, что эти две строки используют cheerio
+  await page.pdf({path: './page-after-click.pdf'})
   //Подбробнее о методе поиска тут https://stackoverflow.com/questions/47407791/how-to-click-on-element-with-text-in-puppeteer
   // const button = await page.$x("//span[contains(., 'Разработка сайтов')]");
   try{
-    await (page.$x("//span[contains(., 'Разработка сайтов')]")).click;
-    // await button.click();
+    await page.addStyleTag({path : './my-css.css'})
+    await page.click('.b-combo__body_left > .b-combo__list > .b-combo__item:first-child');
+    await page.click('.b-layout__right_bordleft_cdd1d3 .b-combo__list > .b-combo__item:first-child');
+    await page.click('.b-layout__right_bordleft_cdd1d3 .b-combo__list > .b-combo__item:first-child > span');
   }
   catch (err){
+    console.log('\n\n-----------Error description beginning-----------\n');
     console.log(err);
+    console.log('\n-----------Error description end-----------\n\n')
     await browser.close();
   }
 
